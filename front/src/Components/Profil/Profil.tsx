cb7203afb92dfbf431e97ffd230cd96a4d287a77
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-05-23 00:14:53 +0100

    J'ai 2200 de io, donc je m'en fiche
    J'ai 2200 de io, donc je m'en fiche

--- front/src/Components/Profil/Profil.tsx
+++ front/src/Components/Profil/Profil.tsx
@@ -7,25 +7,20 @@ export default function Profil() {
   useEffect(() => {
     isAuthorised().then((res) => {
       console.log(res);
-    }).catch((error) => {
-      console.log("Erreur lors de la vérification du token :", error);
     });
   }, []);
 
   async function isAuthorised() {
     const url = serverAddress + "verify-token";
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token, // Ajouter un espace après "Bearer"
-        },
-      });
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      throw new Error("Erreur lors de la requête de vérification du token : " + error);
-    }
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    const responseData = await response.json();
+    return responseData.response;
   }
 
   return (
